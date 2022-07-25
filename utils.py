fix normalization issues

utils.py
@@ -99,5 +99,9 @@ def save_model(model, model_dir):
     fname = 'model.pt'
     torch.save(store, os.path.join(model_dir, fname))
     
-
-
+def normalize_batch(batch):
+    # normalize using imagenet mean and std
+    mean = batch.new_tensor([0.485, 0.456, 0.406]).view(-1, 1, 1)
+    std = batch.new_tensor([0.229, 0.224, 0.225]).view(-1, 1, 1)
+    batch = batch.div_(255.0)
+    return (batch - mean) / std