b1993cda5c5906c12ff1f30915e07e0f9e986c27
thinkin-machine <you@example.com>
2022-07-26 10:28:17 +0530

    fix download and extract

--- neural_style_transfer/get_resources.py
+++ neural_style_transfer/get_resources.py
@@ -5,6 +5,6 @@ from urllib import request
 def download_and_extract(url, fname):
     request.urlretrieve(url, fname)
 
-    if not os.path.exists(fname):
+    if not os.path.exists(fname.split('.')[0]):
         with zipfile.ZipFile(fname, 'r') as f:
             f.extractall()
